f240a6f423cba3eef28e954e000a86c0df9b83c0
gust <2414566866@qq.com>
2024-06-11 10:29:09 +0800

    add module

---
+++ work-flow-engine/demo-flowable/src/test/java/com/gust/cafe/demoflowable/DemoFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gust.cafe.demoflowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
