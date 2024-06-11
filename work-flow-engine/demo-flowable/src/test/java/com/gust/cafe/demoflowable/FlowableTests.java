7bf0c488b6eb06f903ed5b8b5fede564a9291424
gust <2414566866@qq.com>
2024-06-12 00:29:42 +0800

    add

---
+++ work-flow-engine/demo-flowable/src/test/java/com/gust/cafe/demoflowable/FlowableTests.java
@@ -0,0 +1,10 @@
+package com.gust.cafe.demoflowable;
+
+import org.junit.jupiter.api.Test;
+
+public class FlowableTests {
+    @Test
+    public void deploy() {
+        System.out.println("hello");
+    }
+}
