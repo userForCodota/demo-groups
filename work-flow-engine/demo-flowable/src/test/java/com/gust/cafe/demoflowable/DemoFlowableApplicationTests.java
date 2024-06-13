8736292bd22fe31c8d4db8cc04c09fe5b180c7f4
gust <2414566866@qq.com>
2024-06-13 14:44:03 +0800

    flowable部署测试:spring环境

--- work-flow-engine/demo-flowable/src/test/java/com/gust/cafe/demoflowable/DemoFlowableApplicationTests.java
+++ work-flow-engine/demo-flowable/src/test/java/com/gust/cafe/demoflowable/DemoFlowableApplicationTests.java
@@ -1,13 +1,21 @@
 package com.gust.cafe.demoflowable;
 
+import cn.hutool.system.SystemUtil;
+import org.flowable.engine.ProcessEngine;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoFlowableApplicationTests {
+    @Autowired
+    private ProcessEngine processEngine;
 
     @Test
     void contextLoads() {
+        String javaHome = SystemUtil.get("JAVA_HOME");
+        System.out.println(javaHome);
+        // 打印
+        System.out.println(processEngine);
     }
-
 }
