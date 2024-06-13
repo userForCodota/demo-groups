91057878556b4eaa113e9464a91d13abcbc3e936
gust <2414566866@qq.com>
2024-06-13 13:39:23 +0800

    flowable部署测试

--- work-flow-engine/demo-flowable/src/test/java/com/gust/cafe/demoflowable/FlowableTests.java
+++ work-flow-engine/demo-flowable/src/test/java/com/gust/cafe/demoflowable/FlowableTests.java
@@ -4,6 +4,7 @@ import org.flowable.engine.ProcessEngine;
 import org.flowable.engine.ProcessEngineConfiguration;
 import org.flowable.engine.RepositoryService;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.flowable.engine.repository.Deployment;
 import org.junit.jupiter.api.Test;
 
 public class FlowableTests {
@@ -20,8 +21,14 @@ public class FlowableTests {
         cfg.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
         // 构建
         ProcessEngine processEngine = cfg.buildProcessEngine();
+        // 部署
         RepositoryService repositoryService = processEngine.getRepositoryService();
-
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource("process/001_test/firstflow.bpmn20.xml")
+                .name("my_first_flow")
+                .deploy();
+        String deploymentId = deployment.getId();
+        System.out.println("部署ID：" + deploymentId);
 
         System.out.println("结束部署");
     }
