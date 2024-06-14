993dac4846efb8db6b9798f492956b05225c3a95
gust <2414566866@qq.com>
2024-06-14 11:19:14 +0800

    demo-flowable:表达式

---
+++ work-flow-engine/demo-flowable/src/main/java/com/gust/cafe/demoflowable/listener/MyListener01.java
@@ -0,0 +1,17 @@
+package com.gust.cafe.demoflowable.listener;
+
+
+import org.flowable.task.service.delegate.DelegateTask;
+import org.flowable.task.service.delegate.TaskListener;
+
+/**
+ * 请输入类描述
+ *
+ * @author Dororo
+ * @date 2024-06-14 11:15
+ */
+public class MyListener01 implements TaskListener {
+    @Override
+    public void notify(DelegateTask delegateTask) {
+    }
+}
